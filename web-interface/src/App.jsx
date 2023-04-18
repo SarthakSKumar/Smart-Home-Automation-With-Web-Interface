ed71a86b831ebbdc82efed02d49f34e94f1512a5
Sarthak S Kumar <sskworld9742@gmail.com>
2023-04-18 12:03:17 +0530

    Update UI

--- web-interface/src/App.jsx
+++ web-interface/src/App.jsx
@@ -10,11 +10,13 @@ import {
   FaDoorClosed,
   FaDoorOpen,
   FaPowerOff,
+  FaWater,
+  FaFillDrip,
 } from "react-icons/fa";
 import { BiCheckboxSquare, BiCheckbox } from "react-icons/bi";
 
 export default function App() {
-  const [ngrokUrl, setNgrokUrl] = useState("http://0.tcp.in.ngrok.io:12160");
+  const [ngrokUrl, setNgrokUrl] = useState("http://0.tcp.in.ngrok.io:16664");
   const [urlSet, setUrlSet] = useState(false);
 
   const [temperature, setTemperature] = useState(0);
@@ -46,12 +48,11 @@ export default function App() {
   };
 
   useEffect(() => {
-    getData();
-    setInterval(null, 3000);
+    setInterval(getData, 1000);
   });
 
   return (
-    <div className="min-h-screen bg-gray-200 py-6 flex flex-col justify-center sm:py-12">
+    <div className="min-h-screen bg-gray-600 py-6 flex flex-col justify-center sm:py-12">
       {urlSet ? (
         <div className="max-w-2xl mx-auto">
           <form onSubmit={handleUrlSubmit}>
@@ -70,124 +71,102 @@ export default function App() {
             />
             <button
               type="submit"
-              className="mt-4 px-4 py-2 bg-indigo-500 text-white rounded shadow"
+              className="mt-4 px-4 py-2 bg-indigo-500 text-gray-800 rounded shadow"
             >
               Submit
             </button>
           </form>
         </div>
       ) : (
-        <div className="min-h-screen bg-gray-200 py-6 flex flex-col justify-center sm:py-12">
-          <div className="relative py-3 max-w-2xl sm:mx-auto">
-            <div className="absolute inset-0 bg-gradient-to-r from-green-400 to-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-            <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-              <div className="max-w-md mx-auto">
+        <div className="h-fit bg-gray-600 px-6">
+          <div className="w-full sm:mx-auto">
+            <div className="relative py-8 bg-gray-800 shadow-lg sm:rounded-3xl sm:p-20">
+              <div className="w-full mx-auto">
                 <div>
                   <img
                     src="vite.svg"
                     alt="Smart Home Automation"
                     className="h-16 mx-auto"
                   />
-                  <h2 className="text-center text-3xl font-extrabold text-gray-900 mt-4 mb-8">
+                  <h2 className="text-center text-3xl font-bold text-gray-100 mt-4 mb-8">
                     Smart Home Automation Dashboard
                   </h2>
-                  <div className="grid grid-cols-2 gap-6">
-                    <div className="bg-white overflow-hidden shadow rounded-lg">
-                      <div className="px-4 py-5 sm:p-6">
-                        <div className="flex items-center">
+                  <div className="flex w-full flex-wrap flex-row-reverse">
+                    <div className="flex flex-wrap w-1/2 border-2"></div>
+                    <div className="flex flex-wrap w-1/2 border-2">
+                      <div className="w-full grid grid-cols-3">
+                        <div className="flex items-center h-fit bg-gray-700 px-2 py-5 rounded-xl mx-4">
                           <div className="flex-shrink-0 bg-green-500 rounded-md p-3">
-                            <img
-                              src="vite.svg"
-                              alt="Temperature"
-                              className="h-6 w-6"
-                            />
+                            <FaThermometerHalf />
                           </div>
                           <div className="ml-5 w-0 flex-1">
-                            <dt className="text-sm font-medium text-gray-500 truncate">
+                            <dt className="text-sm font-medium text-gray-400 truncate">
                               Temperature
                             </dt>
                             <dd className="flex items-baseline">
-                              <div className="text-2xl font-semibold text-gray-900">
+                              <div className="text-2xl font-semibold text-gray-100">
                                 {temperature}°C
                               </div>
                             </dd>
                           </div>
                         </div>
-                      </div>
-                    </div>
-                    <div className="bg-white overflow-hidden shadow rounded-lg">
-                      <div className="px-4 py-5 sm:p-6">
-                        <div className="flex items-center">
+                        <div className="flex items-center h-fit bg-gray-700 px-4 py-5 rounded-xl mx-4">
                           <div className="flex-shrink-0 bg-blue-500 rounded-md p-3">
-                            <img
-                              src="vite.svg"
-                              alt="Humidity"
-                              className="h-6 w-6"
-                            />
+                            <FaWater />
                           </div>
                           <div className="ml-5 w-0 flex-1">
-                            <dt className="text-sm font-medium text-gray-500 truncate">
+                            <dt className="text-sm font-medium text-gray-400 truncate">
                               Humidity
                             </dt>
                             <dd className="flex items-baseline">
-                              <div className="text-2xl font-semibold text-gray-900">
+                              <div className="text-2xl font-semibold text-gray-100">
                                 {humidity}%
                               </div>
                             </dd>
                           </div>
                         </div>
-                      </div>
-                    </div>
-                    <div className="bg-white overflow-hidden shadow rounded-lg">
-                      <div className="px-4 py-5 sm:p-6">
-                        <div className="flex items-center">
+                        <div className="flex items-center h-fit bg-gray-700 px-4 py-5 rounded-xl mx-4">
                           <div className="flex-shrink-0 bg-yellow-500 rounded-md p-3">
-                            <img
-                              src="vite.svg"
-                              alt="Soil Moisture"
-                              className="h-6 w-6"
-                            />
+                            <FaSeedling />
                           </div>
                           <div className="ml-5 w-0 flex-1">
-                            <dt className="text-sm font-medium text-gray-500 truncate">
+                            <dt className="text-sm font-medium text-gray-400 truncate">
                               Soil Moisture
                             </dt>
                             <dd className="flex items-baseline">
-                              <div className="text-2xl font-semibold text-gray-900">
+                              <div className="text-2xl font-semibold text-gray-100">
                                 {soilMoisture}%
                               </div>
                             </dd>
                           </div>
                         </div>
                       </div>
-                    </div>
-                    <div className="bg-white overflow-hidden shadow rounded-lg">
-                      <div className="px-4 py-5 sm:p-6">
-                        <div className="flex items-center">
-                          <div
-                            className={`flex-shrink-0 ${
-                              !reedState ? "bg-green-500" : "bg-red-500"
-                            } rounded-md p-3`}
-                          >
-                            <img
-                              src="vite.svg"
-                              alt="Door State"
-                              className="h-6 w-6"
-                            />
-                          </div>
-                          <div className="ml-5 w-0 flex-1">
-                            <dt className="text-sm font-medium text-gray-500 truncate">
-                              Door State
-                            </dt>
-                            <dd className="flex items-baseline">
-                              <div
-                                className={`text-2xl font-semibold ${
-                                  !reedState ? "text-green-500" : "text-red-500"
-                                }`}
-                              >
-                                {reedState ? "Open" : "Closed"}
-                              </div>
-                            </dd>
+                      <div className="bg-gray-800 overflow-hidden shadow rounded-lg">
+                        <div className="px-4 py-5 sm:p-6">
+                          <div className="flex items-center">
+                            <div
+                              className={`flex-shrink-0 ${
+                                reedState ? "bg-green-500" : "bg-red-500"
+                              } rounded-md p-3`}
+                            >
+                              {reedState ? <FaDoorClosed /> : <FaDoorOpen />}
+                            </div>
+                            <div className="ml-5 w-0 flex-1">
+                              <dt className="text-sm font-medium text-gray-400 truncate">
+                                Door State
+                              </dt>
+                              <dd className="flex items-baseline">
+                                <div
+                                  className={`text-2xl font-semibold ${
+                                    reedState
+                                      ? "text-green-500"
+                                      : "text-red-500"
+                                  }`}
+                                >
+                                  {!reedState ? "Open" : "Closed"}
+                                </div>
+                              </dd>
+                            </div>
                           </div>
                         </div>
                       </div>
@@ -195,61 +174,61 @@ export default function App() {
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="grid grid-cols-2 gap-6 mt-8">
-            <div className="bg-white overflow-hidden shadow rounded-lg">
-              <div className="px-4 py-5 sm:p-6">
-                <div className="flex items-center">
-                  <div className="flex-shrink-0 bg-purple-500 rounded-md p-3">
-                    <FaPowerOff className="h-6 w-6 text-white" />
-                  </div>
-                  <div className="ml-5 w-0 flex-1">
-                    <dt className="text-sm font-medium text-gray-500 truncate">
-                      Relay 1 Control
-                    </dt>
-                    <dd className="flex items-center">
-                      <button
-                        onClick={() => toggleRelay(1, true)}
-                        className="px-3 py-2 mr-2 bg-green-500 text-white rounded shadow"
-                      >
-                        ON
-                      </button>
-                      <button
-                        onClick={() => toggleRelay(1, false)}
-                        className="px-3 py-2 bg-red-500 text-white rounded shadow"
-                      >
-                        OFF
-                      </button>
-                    </dd>
+              <div className="grid grid-cols-2 gap-6 mt-8">
+                <div className="bg-gray-800 overflow-hidden shadow rounded-lg">
+                  <div className="px-4 py-5 sm:p-6">
+                    <div className="flex items-center">
+                      <div className="flex-shrink-0 bg-purple-500 rounded-md p-3">
+                        <FaPowerOff className="h-6 w-6 text-gray-800" />
+                      </div>
+                      <div className="ml-5 w-0 flex-1">
+                        <dt className="text-sm font-medium text-gray-400 truncate">
+                          Relay 1 Control
+                        </dt>
+                        <dd className="flex items-center">
+                          <button
+                            onClick={() => toggleRelay(1, true)}
+                            className="px-3 py-2 mr-2 bg-green-500 text-gray-800 rounded shadow"
+                          >
+                            ON
+                          </button>
+                          <button
+                            onClick={() => toggleRelay(1, false)}
+                            className="px-3 py-2 bg-red-500 text-gray-800 rounded shadow"
+                          >
+                            OFF
+                          </button>
+                        </dd>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            <div className="bg-white overflow-hidden shadow rounded-lg">
-              <div className="px-4 py-5 sm:p-6">
-                <div className="flex items-center">
-                  <div className="flex-shrink-0 bg-purple-500 rounded-md p-3">
-                    <FaPowerOff className="h-6 w-6 text-white" />
-                  </div>
-                  <div className="ml-5 w-0 flex-1">
-                    <dt className="text-sm font-medium text-gray-500 truncate">
-                      Relay 2 Control
-                    </dt>
-                    <dd className="flex items-center">
-                      <button
-                        onClick={() => toggleRelay(2, true)}
-                        className="px-3 py-2 mr-2 bg-green-500 text-white rounded shadow"
-                      >
-                        ON
-                      </button>
-                      <button
-                        onClick={() => toggleRelay(2, false)}
-                        className="px-3 py-2 bg-red-500 text-white rounded shadow"
-                      >
-                        OFF
-                      </button>
-                    </dd>
+                <div className="bg-gray-800 overflow-hidden shadow rounded-lg">
+                  <div className="px-4 py-5 sm:p-6">
+                    <div className="flex items-center">
+                      <div className="flex-shrink-0 bg-purple-500 rounded-md p-3">
+                        <FaPowerOff className="h-6 w-6 text-gray-800" />
+                      </div>
+                      <div className="ml-5 w-0 flex-1">
+                        <dt className="text-sm font-medium text-gray-400 truncate">
+                          Relay 2 Control
+                        </dt>
+                        <dd className="flex items-center">
+                          <button
+                            onClick={() => toggleRelay(2, true)}
+                            className="px-3 py-2 mr-2 bg-green-500 text-gray-800 rounded shadow"
+                          >
+                            ON
+                          </button>
+                          <button
+                            onClick={() => toggleRelay(2, false)}
+                            className="px-3 py-2 bg-red-500 text-gray-800 rounded shadow"
+                          >
+                            OFF
+                          </button>
+                        </dd>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
